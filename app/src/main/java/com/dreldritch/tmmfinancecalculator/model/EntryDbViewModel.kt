62802d3a49a481fd0e81f2775022642bf0859c85
dreld <drunkenEldritch@gmail.com>
2018-03-19 19:32:25 +0100

    Add DbViewModel and pass account table data to dialog
    - Query account table through ViewModel
    - Set data in adapter for the ListView

---
+++ app/src/main/java/com/dreldritch/tmmfinancecalculator/model/EntryDbViewModel.kt
@@ -0,0 +1,10 @@
+package com.dreldritch.tmmfinancecalculator.model
+
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+
+class EntryDbViewModel(application: Application): AndroidViewModel(application) {
+    val repository = EntryDbRepository(application)
+
+    fun getAllAccounts() = repository.getAllAccounts()
+}
