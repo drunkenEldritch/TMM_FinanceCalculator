6922e8459c8133d1acc8b8ad8cc91cddb3292917
dreld <drunkenEldritch@gmail.com>
2018-03-20 11:40:40 +0100

    Add insert for multiple items to BaseDao

--- app/src/main/java/com/dreldritch/tmmfinancecalculator/model/dao/BaseDao.kt
+++ app/src/main/java/com/dreldritch/tmmfinancecalculator/model/dao/BaseDao.kt
@@ -4,8 +4,12 @@ import android.arch.persistence.room.*
 
 @Dao
 abstract class BaseDao<T>{
+
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    abstract fun insert(obj: T): Long
+    abstract fun insert(obj: T):Long
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    abstract fun insert(vararg obj: T)
 
     @Update(onConflict = OnConflictStrategy.REPLACE)
     abstract fun update(obj: T)
